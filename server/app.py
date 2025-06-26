f215f5acfa386bf1fadb66e6d995c52322d5b971
aminacherop <cheropamina26@gmail.com>
2025-06-26 23:49:06 +0300

    initial commit

--- server/app.py
+++ server/app.py
@@ -5,6 +5,7 @@ from flask_migrate import Migrate
 from models import db, Message
 
 app = Flask(__name__)
+CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
@@ -14,13 +15,60 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
-def messages():
-    return ''
+
+
+@app.route('/messages', methods=['GET'])
+def get_messages():
+    messages = Message.query.order_by(Message.created_at.asc()).all()
+    messages_serialized = [msg.to_dict() for msg in messages]
+    return jsonify(messages_serialized), 200
 
 @app.route('/messages/<int:id>')
 def messages_by_id(id):
     return ''
+
+@app.route('/messages', methods=['POST'])
+def create_message():
+    data = request.get_json() 
+
+    body = data.get('body')
+    username = data.get('username')
+
+    if not body or not username:
+        return jsonify({"error": "Missing 'body' or 'username'"}), 400
+
+    new_message = Message(body=body, username=username)
+    db.session.add(new_message)
+    db.session.commit()
+
+    return jsonify(new_message.to_dict()), 201
+
+@app.route('/messages/<int:id>', methods=['PATCH'])
+def update_message(id):
+    message = db.session.get(Message,id)
+    if not message:
+        return jsonify({"error": "Message not found"}), 404
+
+    data = request.get_json()  
+
+    if 'body' in data:
+        message.body = data['body']
+
+    db.session.commit()
+    return jsonify(message.to_dict()), 200
+
+    return jsonify({"message": "Message successfully deleted"}), 200
+
+@app.route('/messages/<int:id>', methods=['DELETE'])
+def delete_message(id):
+    message = db.session.get(Message, id)
+    if not message:
+        return jsonify({"error": "Message not found"}), 404
+
+    db.session.delete(message)
+    db.session.commit()
+    return jsonify({"message": "Message successfully deleted"}), 200
+
 
 if __name__ == '__main__':
     app.run(port=5555)
